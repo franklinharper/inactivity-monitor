1082a14a287df43be00158f2efd96e14908cfdf8
Frank Harper <frank.harper@gmail.com>
2019-06-24 07:43:26 -0700

    Calculate and display the total duration of the user's latest activity type

--- app/src/main/java/com/franklinharper/inactivitymonitor/MainActivity.kt
+++ app/src/main/java/com/franklinharper/inactivitymonitor/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.franklinharper.inactivitymonitor
 
+import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.location.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.IllegalStateException
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val UNKNOWN_ACTIVITY = "UNKNOWN_ACTIVITY"
+    }
+
+    @Suppress("SpellCheckingInspection")
+    private val todaysLog = StringBuilder()
 
     private val transitions = listOf<ActivityTransition>(
         createActivityTransition(DetectedActivity.IN_VEHICLE, ActivityTransition.ACTIVITY_TRANSITION_ENTER)
@@ -32,42 +38,21 @@ class MainActivity : AppCompatActivity() {
         , createActivityTransition(DetectedActivity.WALKING, ActivityTransition.ACTIVITY_TRANSITION_EXIT)
     )
 
-    val request = ActivityTransitionRequest(transitions)
 
-    private lateinit var textMessage: TextView
+    private var latestActivityType = UNKNOWN_ACTIVITY
+    private var latestActivityMinutes = 0.0
+
     private lateinit var userActivityQueries: UserActivityQueries
 
     private val zoneId = ZoneId.systemDefault()
     private val timeFormatter = DateTimeFormatter.ofPattern("kk:HH:mm:ss").withZone(zoneId)
 
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                showActivitiesForToday()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        textMessage = findViewById(R.id.message)
         userActivityQueries = InactivityDb.from(this).queries
 
-        showActivitiesForToday()
-
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        navigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
         val intent = Intent(this, ActivityTransitionReceiver::class.java)
 
@@ -75,7 +60,8 @@ class MainActivity : AppCompatActivity() {
 
         val client = ActivityRecognition.getClient(this)
 
-        val task = client.requestActivityTransitionUpdates(request, pendingIntent)
+        val activityTransitionRequest = ActivityTransitionRequest(transitions)
+        val task = client.requestActivityTransitionUpdates(activityTransitionRequest, pendingIntent)
 
         task.addOnSuccessListener {
             Log.d("MAIN", "Success")
@@ -84,47 +70,93 @@ class MainActivity : AppCompatActivity() {
         task.addOnFailureListener { e: Exception ->
             Log.d("MAIN", "Fail")
         }
+        calculateTodaysActivities()
+        updateSelectedNavigationItem(navigationView.selectedItemId)
     }
 
     override fun onResume() {
         super.onResume()
-        showActivitiesForToday()
+        calculateTodaysActivities()
+        updateSelectedNavigationItem(navigationView.selectedItemId)
     }
 
-    private fun showActivitiesForToday() {
+    private fun updateSelectedNavigationItem(id:Int) {
+        calculateTodaysActivities()
+        when (id) {
+            R.id.navigation_dashboard -> showDashboardForToday()
+            R.id.navigation_log -> showLogForToday()
+            R.id.navigation_notifications -> showNotificationsForToday()
+            else -> IllegalStateException()
+        }
+    }
+
+    // For faster development, we'll delay implementing I18n until it becomes necessary
+    @SuppressLint("SetTextI18n")
+    private fun showDashboardForToday() {
+        val formattedMinutes = "%.2f".format(latestActivityMinutes)
+        message.text =
+            """
+               
+             Current status:
+             
+             $latestActivityType for the last $formattedMinutes minutes.
+           """.trimIndent()
+    }
+
+    private fun showLogForToday() {
+        message.text = todaysLog.toString()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showNotificationsForToday() {
+        message.text = "Not yet implemented"
+    }
+
+    @Suppress("SpellCheckingInspection")
+    private fun calculateTodaysActivities() {
+
+        todaysLog.clear()
+        val now = System.currentTimeMillis() / 1000
+        val formattedNow = timeFormatter.format(Instant.ofEpochSecond(now))
+        todaysLog.append("Updated $formattedNow\n\n")
         val todayMidnight = LocalDate.now().atStartOfDay(zoneId)
         val tomorrowMidnight = todayMidnight.plusDays(1)
-        val todaysActivities = StringBuilder()
 
-        todaysActivities.append("Activities\n")
-        var previousTimestamp = System.currentTimeMillis() / 1000
-        var previousActivityType = ""
-        var first = true
-        var delta = 0L
+        var previousTimestamp = now
+        var previousActivityType = UNKNOWN_ACTIVITY
+        var currentActivitySecs = 0L
+        var isLatestActivity = true
         userActivityQueries.select(
             transition = ActivityTransition.ACTIVITY_TRANSITION_ENTER,
-            start = todayMidnight.toEpochSecond(),
-            end = tomorrowMidnight.toEpochSecond()
+            startInclusive = todayMidnight.toEpochSecond(),
+            endExclusive = tomorrowMidnight.toEpochSecond()
         )
             .executeAsList()
-            .forEach {
-                with(it) {
+            // Go backwards in time calculating the duration of each successive activity transition
+            .forEach {userActivity ->
+                with(userActivity) {
                     val formattedTimestamp = timeFormatter.format(Instant.ofEpochSecond(timestamp))
-                    val activityType = typeToString(activity_type)
-                    delta = when {
-//                        previousTimestamp == -1L -> 0L
-                        first || activityType == previousActivityType -> {
-                            first = false
-                            delta + previousTimestamp - timestamp
+                    val currentActivityType = activity_type.activityTypeToString()
+                    if (previousActivityType != currentActivityType && previousActivityType != UNKNOWN_ACTIVITY) {
+                        if (isLatestActivity) {
+                            latestActivityType = previousActivityType
+                            latestActivityMinutes = currentActivitySecs / 60.0
+                            isLatestActivity = false
                         }
-                        else -> previousTimestamp - timestamp
+                        // start calculation for the new activity type
+                        currentActivitySecs = 0L
                     }
-                    todaysActivities.append("$formattedTimestamp => $activityType $delta secs\n")
+                    currentActivitySecs += previousTimestamp - timestamp
+                    todaysLog.append("$formattedTimestamp => $currentActivityType $currentActivitySecs secs\n")
                     previousTimestamp = timestamp
-                    previousActivityType = activityType
+                    previousActivityType = currentActivityType
                 }
             }
-        message.text = todaysActivities.toString()
+    }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        updateSelectedNavigationItem(item.itemId)
+        return@OnNavigationItemSelectedListener true
     }
 
     private fun createActivityTransition(type: Int, transition: Int): ActivityTransition {
@@ -142,22 +174,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun typeToString(type: Int): String {
-        return when (type) {
+    private fun Int.activityTypeToString(): String {
+        return when (this) {
             0 -> "IN_VEHICLE"
             1 -> "ON_BICYCLE"
             2 -> "ON_FOOT"
             3 -> "STILL"
-            4 -> "UNKNOWN"
+            4 -> "UNKNOWN_ACTIVITY"
             5 -> "TILTING"
-            6, 9, 10, 11, 12, 13, 14, 15 -> type.toString()
+            6, 9, 10, 11, 12, 13, 14, 15 -> this.toString()
             7 -> "WALKING"
             8 -> "RUNNING"
             16 -> "IN_ROAD_VEHICLE"
             17 -> "IN_RAIL_VEHICLE"
             18 -> "IN_TWO_WHEELER_VEHICLE"
             19 -> "IN_FOUR_WHEELER_VEHICLE"
-            else -> type.toString()
+            else -> this.toString()
         }
     }
 }
