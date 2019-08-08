35f30dc0b8a9006d8f65bdfade7ba4ef978d0d5d
Frank Harper <frank.harper@gmail.com>
2019-08-08 07:56:06 -0700

    google-sql-backend: Add logging configuration

--- inactivity_monitory_google_cloud_sql/src/main/kotlin/HelloApplication.kt
+++ inactivity_monitory_google_cloud_sql/src/main/kotlin/HelloApplication.kt
@@ -1,48 +1,56 @@
 package com.example.demo
 
-import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.html.*
-import io.ktor.routing.*
-import kotlinx.html.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.html.respondHtml
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import kotlinx.html.body
+import kotlinx.html.head
+import kotlinx.html.p
+import kotlinx.html.title
 
 // Entry Point of the application as defined in resources/application.conf.
 // @see https://ktor.io/servers/configuration.html#hocon-file
 fun Application.main() {
-    // This adds Date and Server headers to each response, and allows custom additional headers
-    install(DefaultHeaders)
-    // This uses use the logger to log every call (request/response)
-    install(CallLogging)
+  // This adds Date and Server headers to each response, and allows custom additional headers
+  install(DefaultHeaders)
+  // This uses use the logger to log every call (request/response)
+  install(CallLogging)
 
-    // Registers routes
-    routing {
-        // Here we use a DSL for building HTML on the route "/"
-        // @see https://github.com/Kotlin/kotlinx.html
-        get("/") {
-            call.respondHtml {
-                head {
-                    title { +"Ktor on Google App Engine standard environment" }
-                }
-                body {
-                    p {
-                        +"Hello there! This is Ktor running on App Engine standard environment"
-                    }
-                }
-            }
-        }
-get("/demo") {
-    call.respondHtml {
+  // Registers routes
+  routing {
+    get()
+    // Here we use a DSL for building HTML on the route "/"
+    // @see https://github.com/Kotlin/kotlinx.html
+    get("/") {
+      call.respondHtml {
         head {
-            title { +"Ktor on App Engine standard environment" }
+          title { +"Ktor on Google App Engine standard environment" }
         }
         body {
-            p {
-                +"It's another route!"
-            }
+          p {
+            +"Hello there! This is Ktor running on App Engine standard environment"
+          }
         }
+      }
     }
-}
+    get("/demo") {
+      call.respondHtml {
+        head {
+          title { +"Ktor on App Engine standard environment" }
+        }
+        body {
+          p {
+            +"It's another route!"
+          }
+        }
+      }
+    }
 
-    }
+  }
 }
 
