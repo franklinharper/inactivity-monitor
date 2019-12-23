69869dc3a8023e14ee5524f43e6454fb13bde52c
Frank Harper <frank.harper@gmail.com>
2019-12-23 11:02:54 -0800

    Always write logs to local files
    In addition to logcat and/or network logging.

--- app/src/main/java/com/franklinharper/inactivitymonitor/MyApplication.kt
+++ app/src/main/java/com/franklinharper/inactivitymonitor/MyApplication.kt
@@ -1,19 +1,52 @@
 package com.franklinharper.inactivitymonitor
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.util.Log
+import fr.bipi.tressence.file.FileLoggerTree
 import timber.log.Timber
+import java.io.File
 
+// Suprressing Lint warning about this class being unused.
+@Suppress("unused")
 class MyApplication : Application() {
 
-  // TODO: Each component should have its own initialization instead of sub-classing Application.
+  // TODO: Each component should perform its own initialization instead of sub-classing Application.
   override fun onCreate() {
     super.onCreate()
     AppComponent.instance = AppModule(this)
+
     if (BuildConfig.DEBUG) {
-      Timber.plant(Timber.DebugTree())
+      addDebugLogger()
     } else {
       // TODO configure production logging
       // Timber.plant(CrashReportingTree ())
     }
+    addFileLogger()
+  }
+
+  private fun addDebugLogger() {
+    Timber.plant(Timber.DebugTree())
+  }
+
+  @SuppressLint("LogNotTimber")
+  private fun addFileLogger() {
+    val logDir = File(filesDir, "logs")
+    Log.i("LOGGER", "logDir: $logDir")
+    if (!logDir.exists()) {
+      Log.i("LOGGER", "created logDir")
+      logDir.mkdir()
+    }
+    val fileLogger = FileLoggerTree.Builder()
+      .withFileName("file%g.log")
+      .withDir(logDir)
+      .withSizeLimit(1_000_000)
+      .withFileLimit(3)
+      .withMinPriority(Log.DEBUG)
+      .appendToFile(true)
+      .build()
+    Timber.plant(fileLogger)
+    Timber.d("========================")
+    Timber.d("Logging to files enabled")
   }
 }
