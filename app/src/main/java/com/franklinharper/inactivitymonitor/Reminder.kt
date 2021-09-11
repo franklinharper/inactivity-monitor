d5927e92aed4742945aca487d594c064b4510f19
Frank Harper <frank.harper@gmail.com>
2021-09-11 13:26:19 -0700

    Failed attemp to prevent Reminders when a phone call is active

--- app/src/main/java/com/franklinharper/inactivitymonitor/Reminder.kt
+++ app/src/main/java/com/franklinharper/inactivitymonitor/Reminder.kt
@@ -10,7 +10,8 @@ class Reminder @Inject constructor(
   private val appVibrator: AppVibrator,
   private val notificationSender: NotificationSender,
   private val snooze: Snooze,
-  private val appSettings: AppSettings
+  private val appSettings: AppSettings,
+  private val phoneCall: PhoneCall,
 ) {
 
   fun update() {
@@ -45,11 +46,22 @@ class Reminder @Inject constructor(
     val reminderEnd = appSettings.reminderEnd()
     val tooEarly = hour < reminderStart
     val tooLate = hour >= reminderEnd
-    val still = latestMovement.type == MovementType.STILL_START
-    val tooLong = latestMovement.durationSecs > appSettings.maxStillMinutes() * 60
+    val userIsStill = latestMovement.type == MovementType.STILL_START
+    val userStillForTooLong = userIsStill
+        && latestMovement.durationSecs > appSettings.maxStillMinutes() * 60
+    // FIXME phoneCallIsActive is NOT true when a phone call is active :(
+    val phoneCallIsActive = phoneCall.isActive()
+    Timber.d("phoneCallIsActive $phoneCallIsActive")
     val snoozeEnd = snooze.end
-    Timber.d("reminderStart $reminderStart, reminderEnd $reminderEnd, snoozeEnd $snoozeEnd, still $still, tooLong $tooLong")
-    return still && tooLong && !tooEarly && !tooLate && snoozeEnd == null
+    Timber.d(
+      "reminderStart $reminderStart, reminderEnd $reminderEnd, snoozeEnd $snoozeEnd," +
+          "still $userIsStill, tooLong $userStillForTooLong"
+    )
+    return userStillForTooLong
+        && !tooEarly
+        && !tooLate
+        && snoozeEnd == null
+        && !phoneCallIsActive
   }
 
   private fun remindToMove(movement: UserMovement) {
