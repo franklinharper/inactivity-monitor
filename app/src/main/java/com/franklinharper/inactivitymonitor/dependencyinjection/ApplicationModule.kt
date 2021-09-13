f80db97ea887ab0c8b0c86aee6306f4201e3d27f
Frank Harper <frank.harper@gmail.com>
2021-09-12 19:24:18 -0700

    Upgrade versions: Kotlin and Hilt

--- app/src/main/java/com/franklinharper/inactivitymonitor/dependencyinjection/ApplicationModule.kt
+++ app/src/main/java/com/franklinharper/inactivitymonitor/dependencyinjection/ApplicationModule.kt
@@ -10,14 +10,14 @@ import com.franklinharper.inactivitymonitor.settings.SystemSettings
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import fr.bipi.tressence.file.FileLoggerTree
 import java.io.File
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object ApplicationModule {
 
   @Singleton
